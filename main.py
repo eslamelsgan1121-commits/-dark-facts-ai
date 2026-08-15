26f821d9f4a49ce3691d8a5801877998defbab1b
eslamelsgan1121-commits <eslamelsgan1121@gmail.com>
2026-08-15 14:48:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from google import genai
 client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash',
+    model='gemini-1.5-flash',
     contents='Write a short, dark, and mysterious YouTube script about the deep ocean.'
 )
 
-print("Generated Script:")
+print("Generated Script Successfully:")
 print(response.text)

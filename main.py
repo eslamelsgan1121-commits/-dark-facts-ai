41d04f3b84f4978916b6552e6a44ffd75903f290
eslamelsgan1121-commits <eslamelsgan1121@gmail.com>
2026-08-15 13:50:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 def generate_script(idea):
     prompt = f"Write a professional, dark, and mysterious YouTube script for the topic: {idea}. Target: International audience."
     response = client.models.generate_content(
-        model='gemini-1.5-flash',
+        model='gemini-2.5-flash',
         contents=prompt,
     )
     return response.text

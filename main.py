392c7b9805c965fda57264df43d79f66e1ffd7d5
eslamelsgan1121-commits <eslamelsgan1121@gmail.com>
2026-08-18 18:55:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def generate_youtube_script():
     
     print("=== بداية السكريبت ===")
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+        model="gemini-2.0-flash",
         contents=prompt,
     )
     print(response.text)
